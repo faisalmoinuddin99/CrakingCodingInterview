b4352d9ba11a99044c3484bf40de4b4d51a7b42b
faisal <faisal25march99@gmail.com>
2021-01-10 15:27:44 +0530

    check whether the array contain inside the another array

--- HashTable/src/Main.java
+++ HashTable/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Hashtable;
 
 public class Main {
@@ -11,5 +12,29 @@ public class Main {
         ht.put(3, "Rahul") ;
 
         System.out.println(ht);
+
+        int[] arr1 = {11,5,6,3,7,1} ;
+        int[] arr2 = {3, 5, 110};
+        boolean res = subArray(arr1, arr2) ;
+        System.out.println(res);
+    }
+
+    public static boolean subArray(int arr1[], int arr2[]){
+        int m = arr1.length;
+        int n = arr2.length;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i = 0; i < m; i++){
+            if (!hashSet.contains(arr1[i])){
+                hashSet.add(arr1[i]) ;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if (!hashSet.contains(arr2[i])){
+                return false;
+            }
+        }
+        return  true ;
     }
 }
