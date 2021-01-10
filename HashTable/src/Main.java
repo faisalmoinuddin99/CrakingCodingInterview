f7775f8d8f63641bf894ec175360af71bcd2ab14
faisal <faisal25march99@gmail.com>
2021-01-10 12:15:00 +0530

    Introduction to Hash Table

---
+++ HashTable/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+       // Lets implement Hash Table
+
+        Hashtable<Integer, String> ht = new Hashtable<>();
+
+        ht.put(1, "Faisal");
+        ht.put(2, "Goku") ;
+        ht.put(3, "Rahul") ;
+
+        System.out.println(ht);
+    }
+}
